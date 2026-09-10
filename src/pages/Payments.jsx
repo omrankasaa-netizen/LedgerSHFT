cc6889967128362749e61bb23184f37731f3ac00
omrankasaa-netizen <omran.kasaa@gmail.com>
2026-09-10 23:06:42 +0300

    Migrate Payments page to self-hosted API

--- src/pages/Payments.jsx
+++ src/pages/Payments.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { base44 } from "@/api/base44Client";
+import { db } from "@/api/entities";
 import { PageHeader, Table, Loading, EmptyState } from "@/components/ui/common";
 import { formatMoney } from "@/lib/finance";
 import { Plus, Wallet, Search } from "lucide-react";
@@ -20,9 +20,9 @@ export default function Payments() {
     (async () => {
       try {
         const [pays, custs, inv] = await Promise.all([
-          base44.entities.Payment.list(),
-          base44.entities.Customer.list(),
-          base44.entities.Invoice.list(),
+          db.entities.Payment.list(),
+          db.entities.Customer.list(),
+          db.entities.Invoice.list(),
         ]);
         setPayments(pays);
         setCustomers(custs);
